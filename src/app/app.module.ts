1946e13b69df1219e765b34ccb7aa323e159f415
pasante12 <pasante12@gerardoortiz.com>
2020-07-27 16:41:14 -0500

    vista

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { APPROUTING } from './app.routers';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { UsuarioComponent } from './components/usuario/usuario.component';
+import { BodyComponent } from './components/body/body.component';
+import { LugarComponent } from './components/lugar/lugar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    UsuarioComponent,
+    BodyComponent,
+    LugarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    APPROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routers.ts
@@ -0,0 +1,15 @@
+import {RouterModule, Routes} from '@angular/router';
+import { UsuarioComponent } from './components/usuario/usuario.component';
+import {BodyComponent} from './components/body/body.component';
+import { LugarComponent } from './components/lugar/lugar.component';
+    import { from } from 'rxjs';
+
+
+const ROUTES: Routes = [
+{path: 'home', component: BodyComponent},
+{ path: 'registro', component: UsuarioComponent },
+{path: 'lugar', component: LugarComponent }
+
+];
+
+export const APPROUTING = RouterModule.forRoot(ROUTES);
