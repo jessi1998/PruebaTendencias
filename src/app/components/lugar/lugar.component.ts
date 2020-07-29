8999694ac7904992b58062d03a85ed6e572625de
pasante12 <pasante12@gerardoortiz.com>
2020-07-28 23:47:43 -0500

    interfaz

--- src/app/components/lugar/lugar.component.ts
+++ src/app/components/lugar/lugar.component.ts
@@ -1,16 +1,71 @@
 import { Component, OnInit } from '@angular/core';
+import * as AWS from 'aws-sdk';
+import { async } from '@angular/core/testing';
 @Component({
   selector: 'app-lugar',
   templateUrl: './lugar.component.html',
   styleUrls: ['./lugar.component.scss']
 })
 export class LugarComponent implements OnInit {
+  albumBucketName = 'practica1';
+  s3 = new AWS.S3({
+    apiVersion: '2006-03-01',
+    params: { Bucket: 'practica1' },
+  });
+  showImagen = false;
+  archivo = null;
+  urlImagen = null;
+  error = false;
+  subiendo = false;
+
 
   constructor() {
+     // Inicializar el proveedor de credenciales de Amazon Cognito
+     AWS.config.region = 'us-east-1'; // Región
+     AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+       IdentityPoolId: 'us-east-1:be50ae8d-3cf4-40d1-8db7-e17e54bee2b4',
+     });
    }
 
   ngOnInit(): void {
   }
-  
+  onClickSubir = async (event) => {
+    event.preventDefault();
 
-}
+    if (this.archivo) {
+      try {
+        console.log(this.archivo);
+        this.subiendo = true;
+        const data = await new AWS.S3.ManagedUpload({
+          params: {
+            Bucket: this.albumBucketName,
+            Key: this.archivo.name,
+            Body: this.archivo,
+            ACL: 'public-read',
+          },
+        }).promise();
+
+        this.urlImagen = data.Location;
+        this.subiendo = false;
+        this.showImagen = true;
+      } catch (error) {
+        this.error = true;
+        const bucle = setInterval(() => {
+          this.error = false;
+          clearInterval(bucle);
+        }, 2000);
+      }
+    } else {
+      alert('SELECCIONE UN ARCHIVO');
+    }
+  };
+
+  onChange = (event) => {
+    if (event.target.files.length > 0) {
+      this.archivo = event.target.files[0];
+    }
+  };
+
+  }
+
+
